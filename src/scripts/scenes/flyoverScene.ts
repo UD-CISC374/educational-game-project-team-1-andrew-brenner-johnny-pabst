9da527011d09910d5187a4221751ec3aef0ff57e
Andrew Brenner <Abrenner@Andrews-MacBook-Pro.local>
2020-04-14 22:17:22 -0400

    spawned player sprite to flyover scene

--- src/scripts/scenes/flyoverScene.ts
+++ src/scripts/scenes/flyoverScene.ts
@@ -3,15 +3,23 @@ import ExampleObject from '../objects/exampleObject';
 export default class flyoverScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
   background: Phaser.GameObjects.Image;
+  player: Phaser.Physics.Arcade.Sprite;
 
   constructor() {
     super({ key: 'flyoverScene' });
   }
 
   create() {
+
+    // create background
     this.background = this.add.image(0,0,"flyoverBackground");
     this.background.setOrigin(0,0);
     
+    // create player
+    this.player = this.physics.add.sprite(this.scale.width / 2 - 8, this.scale.height - 64, "player");
+
+
+
   }
 
   update() {
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,10 +4,48 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load_images();
+    this.load_spritesheets();
+  }
+
+  /**
+   * Loads all the spritesheets necessary for application
+   */
+  load_spritesheets(){
+    this.load.spritesheet("player", "assets/spritesheets/fly-animation.png", {
+        frameWidth: 100,
+        frameHeight: 50
+      });
+  }
+
+  /**
+   * Loads all the spritesheets necessary for application
+   */
+  load_images(){
     this.load.image("flyoverBackground", "assets/images/farmBG.jpg");
   }
 
+
+
+
+
   create() {
+    this.create_spritesheets();
     this.scene.start('flyoverScene');
+  }
+
+   /**
+   * Creates all the spritesheets necessary for application
+   */
+  create_spritesheets(){
+    this.anims.create({
+      key: "player",
+      frames: this.anims.generateFrameNumbers("power-up", {
+        start: 0,
+        end: 1
+      }),
+      frameRate: 20,
+      repeat: -1
+    });
   }
 }
