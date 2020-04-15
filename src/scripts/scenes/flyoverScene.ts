fef2840fa543d08bf396a1fe1d434d71abbbcab5
Andrew Brenner <Abrenner@Andrews-MacBook-Pro.local>
2020-04-14 22:57:33 -0400

    user movement added

--- src/scripts/scenes/flyoverScene.ts
+++ src/scripts/scenes/flyoverScene.ts
@@ -4,6 +4,7 @@ export default class flyoverScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
   background: Phaser.GameObjects.Image;
   player: Phaser.Physics.Arcade.Sprite;
+  cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
 
   constructor() {
     super({ key: 'flyoverScene' });
@@ -19,10 +20,36 @@ export default class flyoverScene extends Phaser.Scene {
     this.player = this.physics.add.sprite(this.scale.width / 2 - 8, this.scale.height - 64, "playerFly");
     this.player.play("playerFly");
 
-
+    // setup keyboard input
+    this.cursorKeys = this.input.keyboard.createCursorKeys()
 
   }
 
   update() {
+
+    this.movePlayerManager();
+
+
+  }
+
+movePlayerManager(): void{
+  if(this.cursorKeys.left !== undefined &&
+    this.cursorKeys.right !== undefined &&
+    this.cursorKeys.up !== undefined &&
+    this.cursorKeys.down !== undefined){
+    
+    if (this.cursorKeys.left.isDown){
+      this.player.setVelocityX(-200);
+      this.player.setVelocityY(0);
+    } else if(this.cursorKeys.right.isDown){
+      this.player.setVelocityX(200);
+      this.player.setVelocityY(0);
+    } else if(this.cursorKeys.up.isDown){
+      this.player.setVelocityY(-200);
+      this.player.setVelocityX(0);
+    } else if(this.cursorKeys.down.isDown){
+      this.player.setVelocityY(200);
+      this.player.setVelocityX(0);
+    }
   }
 }
