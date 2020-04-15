954a951cfb747daf1a8e94e47f53545d3b348e22
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-04-14 21:50:03 -0400

    background added

--- src/scripts/scenes/flyoverScene.ts
+++ src/scripts/scenes/flyoverScene.ts
@@ -2,13 +2,21 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class flyoverScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  background: Phaser.GameObjects.Image;
 
   constructor() {
     super({ key: 'flyoverScene' });
   }
 
+  preload(){
+    this.load.image("flyoverBackground", "assets/images/farmBG.jpg");
+
+  }
+
   create() {
-    this.exampleObject = new ExampleObject(this, 0, 0);
+    this.background = this.add.image(0,0,"flyoverBackground");
+    this.background.setOrigin(0,0);
+    
   }
 
   update() {
