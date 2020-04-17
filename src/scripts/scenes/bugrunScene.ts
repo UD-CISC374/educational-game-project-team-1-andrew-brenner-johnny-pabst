ec0574eed1886d555c72926bec747983c327ece7
Andrew Brenner <Abrenner@Andrews-MacBook-Pro.local>
2020-04-16 20:12:56 -0400

    bugrunScene features added (nothing displays)

--- src/scripts/scenes/bugrunScene.ts
+++ src/scripts/scenes/bugrunScene.ts
@@ -1,16 +1,75 @@
-import ExampleObject from '../objects/exampleObject';
+import { gameSettings } from "../game";
 
-export default class bugrunScene extends Phaser.Scene {
-  private exampleObject: ExampleObject;
+class bugrunScene extends Phaser.Scene {
+  background: Phaser.GameObjects.TileSprite;
+  player: Phaser.Physics.Arcade.Sprite;
+  cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
 
   constructor() {
     super({ key: 'bugrunScene' });
   }
 
   create() {
-    this.exampleObject = new ExampleObject(this, 0, 0);
+
+    this.add.text(50,50, "loading...", {font: "32px Arial", fill: "black"});
+
+    /*
+    // create background
+    this.background = this.add.tileSprite(0,0, this.scale.width, this.scale.height, "bugrunBackground");
+    this.background.setOrigin(0,0);
+
+    // create player
+    this.player = this.physics.add.sprite(this.scale.width / 2 - 8, this.scale.height - 64, "playerFly");
+    this.player.play("playerFly");
+    this.player.setCollideWorldBounds(true);
+
+    // setup keyboard input
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
+    */
   }
 
+
+/*
   update() {
+    //this.background.tilePositionY -= 0.5; // scroll background
+    //this.movePlayerManager(); // listen for player movement
   }
+*/  
+
+
+
+  /**
+   * Keyboard logic to move the player
+   * Allows diagonal movement
+   */
+  /*
+  movePlayerManager(){
+    // avoid 'Object possibly undefined' errors
+    if(this.cursorKeys.left !== undefined &&
+      this.cursorKeys.right !== undefined &&
+      this.cursorKeys.up !== undefined &&
+      this.cursorKeys.down !== undefined){
+    
+      if(this.cursorKeys.left.isDown){
+        this.player.setVelocityX(-gameSettings.playerSpeed);
+        console.log("left");
+      } else if(this.cursorKeys.right.isDown){
+        this.player.setVelocityX(gameSettings.playerSpeed);
+        console.log("right");
+      } else {
+        this.player.setVelocityX(0); // stop movement when key is released
+      }
+
+      if(this.cursorKeys.up.isDown){
+        this.player.setVelocityY(-gameSettings.playerSpeed);
+        console.log("up");
+      } else if(this.cursorKeys.down.isDown){
+        this.player.setVelocityY(gameSettings.playerSpeed);
+        console.log("down");
+      } else{
+        this.player.setVelocityY(0); // stop movement when key is released
+      }
+    }
+  }
+*/
 }
