8e958578fc9a177c261540ec77a8f54d46ca45c7
Andrew Brenner <Abrenner@Andrews-MacBook-Pro.local>
2020-05-03 20:59:43 -0400

    Updates

---
+++ beta/precache-manifest.e22fcb79944df8a5713707f11e819470.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33853a9453aab118b5ec4032733b1d1b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/.DS_Store"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "d4d33c68b71dcf8138ec5f8928a40c04",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/blackBar.png"
+  },
+  {
+    "revision": "287a126b5c2e611904d95929a5f6da6b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/buzzCapone.png"
+  },
+  {
+    "revision": "69057b31d006c901cc155ac0e7edd60b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/checkMark.png"
+  },
+  {
+    "revision": "ebd753b433bf8e99e000a0f03ad4b8f8",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/cherryTree.png"
+  },
+  {
+    "revision": "e0528688686c82a759c4d5078a352248",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/closeButton.png"
+  },
+  {
+    "revision": "ec0e7c145a08d26694bea45b7b39e07f",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/deadBlackWalnut.png"
+  },
+  {
+    "revision": "f0b22480e178fc21ab8a5ddbee1a0b62",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/deadCherryTree.png"
+  },
+  {
+    "revision": "ba3b9b16cda8d7932cb3906c96068239",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/deadGrapeVine.png"
+  },
+  {
+    "revision": "f564784079696fd5ff4425fdb2888b0c",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/deadTree1.png"
+  },
+  {
+    "revision": "a5b94a36a8a6d38cf648f5455daef94b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/deadTree2.png"
+  },
+  {
+    "revision": "06f0885b739596116b71ccab90fa2bfe",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/deadTree3.png"
+  },
+  {
+    "revision": "5f24f5b0a553251f13527d0b1b065609",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/deadTreeOfHeaven.png"
+  },
+  {
+    "revision": "4bb1a88a040778406caaebb5e70e0a9b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/eggZone.png"
+  },
+  {
+    "revision": "415b89d8481bd095fc783302d9332f32",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/farmBG.jpg"
+  },
+  {
+    "revision": "658c2b052193204d66b6c83d20c2492b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/feedSpot.png"
+  },
+  {
+    "revision": "32ac9d629e4f75ba5ae28a4a3de03e37",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/grapeVine.png"
+  },
+  {
+    "revision": "af93295404e1a084240e97b37eebae41",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/messageBox.png"
+  },
+  {
+    "revision": "e6b41d09b17d02635a16f4404739b318",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/treeBark.jpg"
+  },
+  {
+    "revision": "6ab8805291634f31a0cd2bdd68d4f7a2",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/images/treeTwo.png"
+  },
+  {
+    "revision": "37598dab870bab72734fe7396a88ee4e",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/sounds/bumbleBoogie.mp3"
+  },
+  {
+    "revision": "8184a17ed4b29e67db8c18ddadabad19",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/sounds/death.ogg"
+  },
+  {
+    "revision": "52f8fd26b815d637b79d8bd3889d11c2",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/sounds/farmSounds.ogg"
+  },
+  {
+    "revision": "faebabe580209b8bdd3ad8ab8b82fcb7",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/sounds/munch.ogg"
+  },
+  {
+    "revision": "e235d114aca414ca91de21d7d6fc653c",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/sounds/pop.ogg"
+  },
+  {
+    "revision": "3bc855aff87b99909a672de1e33614e9",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/sounds/spray.ogg"
+  },
+  {
+    "revision": "8eba7ef896a38ca203e7bd2f8887693a",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/sounds/tutorialJam.ogg"
+  },
+  {
+    "revision": "c423a7bf00221346165ef1bd84f114ed",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/appleTreeAnim.png"
+  },
+  {
+    "revision": "e9603461eed2a234cb997fd0a0deb7a6",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/arrow.png"
+  },
+  {
+    "revision": "d442fa95041683d8910d079482635c22",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/blackWalnut.png"
+  },
+  {
+    "revision": "f7a92826240cb5b0cd9be3754ab1818b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/closedFly.png"
+  },
+  {
+    "revision": "d1e4209984d96f6922a926a2039e2b23",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/egg.png"
+  },
+  {
+    "revision": "d0d42702d7e80a3a8a5047d157be272c",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/fly-animation.png"
+  },
+  {
+    "revision": "286c60910791be43efd00d3a00bfd1e7",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/fly-animation2.png"
+  },
+  {
+    "revision": "3a3d1e08d084a21136d4c69abadc1ba0",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/flyAnimationREAL.png"
+  },
+  {
+    "revision": "9c6a058f53b38209df218fb3f71a9b9b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/pesticideWarning.png"
+  },
+  {
+    "revision": "179db2201e44ea16766518a5a4e4ec86",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/pesticideZone.png"
+  },
+  {
+    "revision": "c8eb6f75abaceac78c2ee4ca95c77005",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/prayingMantisLeft.png"
+  },
+  {
+    "revision": "07145e25f00bb9f163a46b96c6cbf2a2",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/prayingMantisRight.png"
+  },
+  {
+    "revision": "c07a9d83738419f963a287560ed606e0",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaassets/spritesheets/treeOfHeaven.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betafavicon.ico"
+  },
+  {
+    "revision": "63b52a0aa1c3dec62b1d",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betagen.main.fe0ee72de21e18e2d21f.bundle.js"
+  },
+  {
+    "revision": "9ecf88fe9811c5d77969",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betagen.vendors.be91aecff310ec1b870c.bundle.js"
+  },
+  {
+    "revision": "018a00421b06b80731922c5d206ba6df",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaindex.html"
+  },
+  {
+    "revision": "51380b2c54906d05cabc1d6e29dd66fb",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betamanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/betasw.js"
+  }
+]);
---
+++ beta/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-1-andrew-brenner-johnny-pabst/betaprecache-manifest.e22fcb79944df8a5713707f11e819470.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
