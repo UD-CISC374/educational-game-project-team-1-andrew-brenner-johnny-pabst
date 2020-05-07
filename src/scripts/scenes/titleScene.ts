aa767b08f2149e2469864d1d0fbba03f2b5f7bdf
johnpabst <60942062+johnpabst@users.noreply.github.com>
2020-05-07 14:16:23 -0400

    title scene created

---
+++ src/scripts/scenes/titleScene.ts
@@ -0,0 +1,14 @@
+
+export default class titleScene extends Phaser.Scene {
+
+    constructor() {
+        super({ key: 'titleScene' });
+      }
+    
+    create(){
+
+    }
+    update(){
+    
+    }
+}
