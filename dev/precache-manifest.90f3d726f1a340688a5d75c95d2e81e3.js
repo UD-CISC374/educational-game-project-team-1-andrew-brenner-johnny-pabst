8ae8b0c7c1839e308a4e665804dca1914820de12
Andrew Brenner <Abrenner@Andrews-MacBook-Pro.local>
2020-05-07 17:25:11 -0400

    Updates

---
+++ dev/precache-manifest.90f3d726f1a340688a5d75c95d2e81e3.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f182072149384388c5a60bb8742a2ae2",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/.DS_Store"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/citations.md"
+  },
+  {
+    "revision": "2d5026fc4151048414261d1b21678f2f",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/font/font.fnt"
+  },
+  {
+    "revision": "4d4c18ed36821411a6453481aa046002",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/font/font.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "d4d33c68b71dcf8138ec5f8928a40c04",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/blackBar.png"
+  },
+  {
+    "revision": "287a126b5c2e611904d95929a5f6da6b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/buzzCapone.png"
+  },
+  {
+    "revision": "69057b31d006c901cc155ac0e7edd60b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/checkMark.png"
+  },
+  {
+    "revision": "ebd753b433bf8e99e000a0f03ad4b8f8",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/cherryTree.png"
+  },
+  {
+    "revision": "e0528688686c82a759c4d5078a352248",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/closeButton.png"
+  },
+  {
+    "revision": "06f0885b739596116b71ccab90fa2bfe",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/deadAppleTree.png"
+  },
+  {
+    "revision": "ec0e7c145a08d26694bea45b7b39e07f",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/deadBlackWalnut.png"
+  },
+  {
+    "revision": "f0b22480e178fc21ab8a5ddbee1a0b62",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/deadCherryTree.png"
+  },
+  {
+    "revision": "ba3b9b16cda8d7932cb3906c96068239",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/deadGrapeVine.png"
+  },
+  {
+    "revision": "5f24f5b0a553251f13527d0b1b065609",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/deadTreeOfHeaven.png"
+  },
+  {
+    "revision": "4bb1a88a040778406caaebb5e70e0a9b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/eggZone.png"
+  },
+  {
+    "revision": "415b89d8481bd095fc783302d9332f32",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/farmBG.jpg"
+  },
+  {
+    "revision": "658c2b052193204d66b6c83d20c2492b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/feedSpot.png"
+  },
+  {
+    "revision": "32ac9d629e4f75ba5ae28a4a3de03e37",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/grapeVine.png"
+  },
+  {
+    "revision": "af93295404e1a084240e97b37eebae41",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/messageBox.png"
+  },
+  {
+    "revision": "dc7cc2d6cab13e0fe07ddc8456497e3d",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/titleScreen.png"
+  },
+  {
+    "revision": "4d73a6d47e1d24f3c3b18a9f7fa88cfe",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/titleStart.png"
+  },
+  {
+    "revision": "e6b41d09b17d02635a16f4404739b318",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/treeBark.jpg"
+  },
+  {
+    "revision": "b7d75bb0d4c4981885190271379812d1",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/images/treeLabel.png"
+  },
+  {
+    "revision": "37598dab870bab72734fe7396a88ee4e",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/sounds/bumbleBoogie.mp3"
+  },
+  {
+    "revision": "8184a17ed4b29e67db8c18ddadabad19",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/sounds/death.ogg"
+  },
+  {
+    "revision": "52f8fd26b815d637b79d8bd3889d11c2",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/sounds/farmSounds.ogg"
+  },
+  {
+    "revision": "faebabe580209b8bdd3ad8ab8b82fcb7",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/sounds/munch.ogg"
+  },
+  {
+    "revision": "e235d114aca414ca91de21d7d6fc653c",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/sounds/pop.ogg"
+  },
+  {
+    "revision": "3bc855aff87b99909a672de1e33614e9",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/sounds/spray.ogg"
+  },
+  {
+    "revision": "8eba7ef896a38ca203e7bd2f8887693a",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/sounds/tutorialJam.ogg"
+  },
+  {
+    "revision": "c423a7bf00221346165ef1bd84f114ed",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/appleTreeAnim.png"
+  },
+  {
+    "revision": "e9603461eed2a234cb997fd0a0deb7a6",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/arrow.png"
+  },
+  {
+    "revision": "d442fa95041683d8910d079482635c22",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/blackWalnut.png"
+  },
+  {
+    "revision": "f7a92826240cb5b0cd9be3754ab1818b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/closedFly.png"
+  },
+  {
+    "revision": "d1e4209984d96f6922a926a2039e2b23",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/egg.png"
+  },
+  {
+    "revision": "d0d42702d7e80a3a8a5047d157be272c",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/fly-animation.png"
+  },
+  {
+    "revision": "286c60910791be43efd00d3a00bfd1e7",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/fly-animation2.png"
+  },
+  {
+    "revision": "3a3d1e08d084a21136d4c69abadc1ba0",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/flyAnimationREAL.png"
+  },
+  {
+    "revision": "9c6a058f53b38209df218fb3f71a9b9b",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/pesticideWarning.png"
+  },
+  {
+    "revision": "179db2201e44ea16766518a5a4e4ec86",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/pesticideZone.png"
+  },
+  {
+    "revision": "c8eb6f75abaceac78c2ee4ca95c77005",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/prayingMantisLeft.png"
+  },
+  {
+    "revision": "07145e25f00bb9f163a46b96c6cbf2a2",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/prayingMantisRight.png"
+  },
+  {
+    "revision": "35991591bf72567b7849e3abbbedad5e",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/sadBird.png"
+  },
+  {
+    "revision": "c07a9d83738419f963a287560ed606e0",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devassets/spritesheets/treeOfHeaven.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devfavicon.ico"
+  },
+  {
+    "revision": "3c20e600aeb6f5dd43f0",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devgen.main.c940dd3328ecf94e70c0.bundle.js"
+  },
+  {
+    "revision": "9ecf88fe9811c5d77969",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devgen.vendors.be91aecff310ec1b870c.bundle.js"
+  },
+  {
+    "revision": "3a57794fc5a4983a8ecd52e5e5028007",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devindex.html"
+  },
+  {
+    "revision": "51380b2c54906d05cabc1d6e29dd66fb",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/educational-game-project-team-1-andrew-brenner-johnny-pabst/devsw.js"
+  }
+]);
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-1-andrew-brenner-johnny-pabst/devprecache-manifest.90f3d726f1a340688a5d75c95d2e81e3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
