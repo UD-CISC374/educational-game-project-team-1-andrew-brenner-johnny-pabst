e41386853751a44e610011a4b1bdd7f6dc33ffc5
Andrew Brenner <Abrenner@Andrews-MacBook-Pro.local>
2020-05-18 16:42:46 -0400

    Updates

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-game-project-team-1-andrew-brenner-johnny-pabst/devprecache-manifest.90f3d726f1a340688a5d75c95d2e81e3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-team-1-andrew-brenner-johnny-pabst/devprecache-manifest.ed1a49b05d34c3262c7aaf88e07870ae.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
